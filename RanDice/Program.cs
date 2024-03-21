3dedd46187e3eff2132c8dbcbbe4c7635b3c6de5
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-21 00:03:27 +0000

    Soma de números aleatórios de dados dado pelo utilizador

---
+++ RanDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_dados = int.Parse(args[0]);
+            int semente = int.Parse(args[1]);
+
+            Random random = new Random(semente);
+            int soma = 0;
+
+            for (int i = 0; i < n_dados; i++)
+            {
+                int dado = random.Next(1,7);
+                soma += dado;
+            }
+
+            Console.WriteLine(soma);
+            }
+    }
+}
